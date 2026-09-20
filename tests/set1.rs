f3c1a7bb0df98da9b8c1b5332c3da3d32314d24e
Furanyl42 <yggparallax@gmail.com>
2026-09-21 00:34:11 +0200

    updated set1 chal5 to work on str directly and not files

--- tests/set1.rs
+++ tests/set1.rs
@@ -69,9 +69,8 @@ mod tests {
 
     #[test]
     fn test_challenge_5_implement_repeating_key_xor() {
-        let expected = "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272
-a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f";
-        let message = include_str!("5.txt");
+        let expected = "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f";
+        let message = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal";
         let result = repeating_key_xor(message.as_bytes(), b"ICE");
 
         let plaintext = bytes_to_hex(&result);
